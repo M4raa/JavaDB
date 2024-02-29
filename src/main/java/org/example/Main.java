5462b45a5d020b56a71fdcc3fbcdd41834bcee18
M4raa <davidmarpons@gmail.com>
2024-02-29 10:38:38 +0100

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,43 @@
 package org.example;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static  java.sql.Connection con;
+    public static void main(String[] args) throws SQLException {
+        String host = "jdbc:sqlite:src/main/resources/network";
+        con = java.sql.DriverManager.getConnection( host );
+        insertUser();
     }
+    private static void crearTabla() throws SQLException{
+        Statement st = con.createStatement();
+        st.executeUpdate("CREATE TABLE T2 (c1 VARCHAR(50))");
+        st.close();
+    }
+    private static void leerUsuarios() throws SQLException{
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM usuarios");
+        while (rs.next()){
+            System.out.println("ID: " + rs.getInt(1));
+        }
+    }
+    private static void insertUser(){
+        Statement st = null;
+        String sql = "INSERT INTO usuarios (nombre, apellidos) VALUES ('Janet', 'Espinosa')";
+        try {
+            st = con.createStatement();
+            st.executeUpdate(sql);
+        }catch (SQLException e){
+            System.out.println("Se ha producido un error al insertar el usuario. Mensaje: " + e.getMessage());
+        }finally {
+            try {
+                if (st !=null) st.close();
+            }catch (java.sql.SQLException ex){
+                System.out.println("Se ha producido un error: " + ex.getMessage());
+            }
+        }
+    }
+
 }
